7ee35192987d643e43a57902b49659eabcafc31f
Leianne Louis <leilouis112@gmail.com>
2018-06-30 22:04:39 +0000

    finished revealing items on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,8 @@
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScoll from './modules/RevealOnScoll'
+import $ from 'jquery'
 
 var mobileMenu = new MobileMenu();
+
+new RevealOnScoll($('.feature-item'), '85%');
+new RevealOnScoll($('.testimonial'), '75%');
