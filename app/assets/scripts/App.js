ff1690333471fc46e174977d938d0647f193c994
Leianne Louis <leilouis112@gmail.com>
2018-07-03 02:28:06 +0000

    completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScoll from './modules/RevealOnScoll'
+import StickyHeader from './modules/StickyHeader'
 import $ from 'jquery'
 
 var mobileMenu = new MobileMenu();
 
 new RevealOnScoll($('.feature-item'), '85%');
 new RevealOnScoll($('.testimonial'), '75%');
+var stickyHeader = new StickyHeader();
